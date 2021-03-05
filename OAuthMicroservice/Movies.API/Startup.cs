a610e2bc9e0b87aefcd0d5ef166d79de4dcd60f4
Yadav Dhakal <yadavdhkl@gmail.com>
2021-03-05 12:10:55 -0600

    Movie API, Model and In-Memory Database setup

--- OAuthMicroservice/Movies.API/Startup.cs
+++ OAuthMicroservice/Movies.API/Startup.cs
@@ -1,4 +1,4 @@
-using Microsoft.AspNetCore.Builder;
+﻿using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
@@ -11,6 +11,8 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Movies.API.Data;
 
 namespace Movies.API
 {
@@ -32,6 +34,8 @@ namespace Movies.API
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "Movies.API", Version = "v1" });
             });
+
+            services.AddDbContext<MoviesContext>(options => options.UseInMemoryDatabase("Movies"));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
